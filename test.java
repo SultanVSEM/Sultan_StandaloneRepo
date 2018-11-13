6f3623046c45284fc61a6d35ef527edf7b3e4eb2
Sultan Sikkandar <ssikkand@ford.com>
2018-11-13 14:10:53 -0500

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,22 @@
+/**
+* @wi.implements elibrary/EL-115:164 Returns author of this book
+*/
+/* This is a simple Java program. 
+FileName : "HelloWorld.java". */
+class HelloWorld 
+{ 
+	// Your program begins with a call to main(). 
+	// Prints "Hello, World" to the terminal window. 
+	public static void main(String args[]) 
+	{ 
+		System.out.println("Hello, World"); 
+	} 
+} 
 
+/**
+* {@wi.implements elibrary_alm/EL-122 messages can take up 
+* several lines in the code. Just surround the whole parsing syntax 
+* with {tested}  
+* Anything commented after will not be included.
+*/ 
+      
